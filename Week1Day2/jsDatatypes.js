0d84737a328aa7f9c682169a330fd2f8a9154088
Chandrababu1218 <107299643+Chandrababu1218@users.noreply.github.com>
2026-08-11 16:16:17 +0530

    Add files via upload

---
+++ Week1Day2/jsDatatypes.js
@@ -0,0 +1,37 @@
+var firstName;
+firstName = "Chandra";
+console.log(firstName + ":", typeof firstName + " DataType"); // Sting
+
+var companyName;
+companyName = "TestLeaf";
+console.log(companyName + ":", typeof companyName + " DataType"); // Sting
+
+var phonenumber;
+phonenumber = 1234567890;
+console.log(phonenumber + ":", typeof phonenumber + " DataType"); // Number
+
+var isAutomation;
+isAutomation = true;
+console.log(isAutomation + ":", typeof isAutomation + " Datatype"); // Boolean
+
+var largeNumber;
+largeNumber = 1234567890n;
+console.log(largeNumber + ":", typeof largeNumber + " Datatype"); //BigInt
+
+var hasPlaywright;
+console.log("hasPlaywright", +typeof hasPlaywright + " Datatype"); // NaN
+
+var acctno = null;
+console.log("acctno", +":", typeof acctno + " Datatype");
+// Null (Object Datatype as per the backward compatability of JS
+
+var accountNumber;
+console.log(accountNumber, +typeof +" Datatype"); // Undefined
+
+/* let course = "Selenium";
+course = "Playwright";
+
+console.log(course);
+
+const num = 1234;
+console.log(num); */
